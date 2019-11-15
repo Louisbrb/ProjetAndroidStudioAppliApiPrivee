c539a9a8430fc083575b8641237cb04c715fbf56
josselin <josselin@exemple.com>
2019-11-15 12:15:00 +0100

    Ajout vue liste resultat

--- app/src/main/java/com/example/monprojet/ActivityListeResultat.java
+++ app/src/main/java/com/example/monprojet/ActivityListeResultat.java
@@ -1,13 +1,23 @@
 package com.example.monprojet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 public class ActivityListeResultat extends AppCompatActivity {
+
+    ArrayAdapter<List> monAdapteur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_resultat);
+
+        /*monAdapteur = new ArrayAdapter<List>(MainActivity.this,
+                android.R.layout.future_liste_item,
+                );*/
     }
 }
